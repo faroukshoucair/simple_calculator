e739734b9a7acd7d480fd813e9128bd439f15c26
Farouk Shoucair <148397783+faroukshoucair@users.noreply.github.com>
2026-05-23 11:22:55 -0500

    add README and fixed bugs

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Calculator
+
+This calculator handles simple arithmetic operations.
+
+## Current Features
+* supports single operation arithmetic: addition, subtraction, multiplication, division , and modulus.
+* modern looking UI.
+
+## Future improvements
+* multi-operation expressions (PEMDAS): upgrade the calculator to handle complex, multi-step math strings (e.g., `5 + 3 * (10 / 2)`). 
--- script.js
+++ script.js
@@ -7,6 +7,7 @@ operator = "";
 calcInputs.addEventListener("click", function () {
   input = event.target.innerText;
   switch (input) {
+    case "0":
     case "1":
     case "2":
     case "3":
@@ -33,11 +34,21 @@ calcInputs.addEventListener("click", function () {
       currentNumber = "";
       break;
     case "⌫":
-      currentNumber = currentNumber.substring(0, currentNumber.length - 1);
+      //currentNumber would be an empty string after operator is entered
+      console.log(currentNumber);
       if (currentNumber === "") {
         currentNumber = "0";
+        console.log(prevNumber);
+        calcOutput.value = prevNumber + operator;
+        prevNumber = prevNumber.substring(0, prevNumber.length);
+        calcOutput.value = prevNumber;
+        console.log(prevNumber);
       }
-      calcOutput.value = currentNumber;
+      else {
+        currentNumber = currentNumber.substring(0, currentNumber.length - 1);
+        calcOutput.value = currentNumber;
+      }
+
       break;
     case "AC":
       currentNumber = "";
@@ -54,25 +65,24 @@ calcInputs.addEventListener("click", function () {
       break;
     case "+/-":
       currentNumber = currentNumber * -1;
-      calcOutput.value = currentNumber;
+      calcOutput.value = prevNumber + operator + "(" + currentNumber + ")";
       break;
     case "=":
       if (operator === "+") {
-        add(prevNumber, currentNumber);
+        currentNumber = add(prevNumber, currentNumber);
       }
       else if (operator === "-") {
-        subtract(prevNumber, currentNumber);
+        currentNumber = subtract(prevNumber, currentNumber);
       }
       else if (operator === "×") {
-        multiply(prevNumber, currentNumber);
+        currentNumber = multiply(prevNumber, currentNumber);
       }
       else if (operator === "%") {
-        modulo(prevNumber, currentNumber);
+        currentNumber = modulo(prevNumber, currentNumber);
       }
       else {
-        divide(prevNumber, currentNumber);
+        currentNumber = divide(prevNumber, currentNumber);
       }
-      currentNumber = "";
       prevNumber = "";
       operator = "";
       break;
@@ -81,28 +91,109 @@ calcInputs.addEventListener("click", function () {
   }
 });
 
+/*
+let currentNumber = "";
+let inputStatement = [];
+let operators = [];
+calcInputs.addEventListener("click", function () {
+  input = event.target.innerText;
+  switch (input) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "÷":
+    case "×":
+    case "-":
+    case "+":
+    case "%":
+    case ".":
+      if (input === "×" || input === "-" || input === "+" || input === "÷" || input === "%") {
+        inputStatement.push(currentNumber);
+        inputStatement.push(input);
+        calcOutput.value = inputStatement.join("");
+        currentNumber = "";
+      }
+      else {
+        currentNumber += input;
+        calcOutput.value = inputStatement.join("") + currentNumber;
+      }
+      break;
+    case "⌫":
+      if (currentNumber !== "") {
+        console.log(currentNumber);
+        currentNumber = currentNumber.slice(0, -1);
+        console.log(currentNumber);
+        calcOutput.value = inputStatement.join("") + currentNumber;
+      }
+      else {
+        lastToken = inputStatement.at(inputStatement.length);
+        if (lastToken !== "+" || lastToken !== "-" || lastToken !== "×" || lastToken !== "÷" || lastToken !== "%") {
+          currentNumber = lastToken;
+          currentNumber = currentNumber.slice(0, -1);
+          console.log(currentNumber);
+          calcOutput.value = inputStatement.join("") + currentNumber;
+        }
+        else {
+          console.log(inputStatement);
+          inputStatement.pop();
+          console.log(inputStatement);
+          calcOutput.value = inputStatement.join("");
+        }
+      }
+      break;
+    case "AC":
+      inputStatement = [];
+      calcOutput.value = inputStatement.join("");
+      break;
+    case "+/-":
+      let num = parseFloat(inputStatement.at(inputStatement.length));
+      inputStatement.pop();
+      inputStatement.push("(" + (num * -1) + ")");
+      calcOutput.value = inputStatement.join("");
+      break;
+    case "=":
+      inputStatement.push(currentNumber);
+    default:
+      break;
+
+
+  }
+});*/
+
 
 function add(a, b) {
   let ans = parseFloat(a) + parseFloat(b);
   calcOutput.value = ans;
+  return ans;
 }
 
 function subtract(a, b) {
   let ans = parseFloat(a) - parseFloat(b);
   calcOutput.value = ans;
+  return ans;
 }
 
 function multiply(a, b) {
   let ans = parseFloat(a) * parseFloat(b);
   calcOutput.value = ans;
+  return ans;
 }
 
 function divide(a, b) {
   let ans = parseFloat(a) / parseFloat(b);
   calcOutput.value = ans;
+  return ans;
 }
 
 function modulo(a, b) {
   let ans = parseFloat(a) % parseFloat(b);
   calcOutput.value = ans;
+  return ans;
 }
